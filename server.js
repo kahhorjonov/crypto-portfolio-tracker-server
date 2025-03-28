c189c8612a7ab3df7d3bdb8880594bf267ead098
kahhorjonov <akbarjon3028@gmail.com>
2025-03-28 14:04:48 +0500

    logging server requests

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use("/portfolio", portfolioRoutes);
 app.use("/add-coin", portfolioRoutes);
 app.use("/remove-coin", portfolioRoutes);
 
-const server = app.listen(port, "0.0.0.0", () => {
+const server = app.listen(PORT, "0.0.0.0", () => {
   console.log(`HTTP Server http://0.0.0.0:${port} da ishlamoqda`);
   console.log("Listening on port:", server.address().port);
   console.log("Address:", server.address().address);
